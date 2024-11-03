105086baeed9d2994b4ae179bcdeb95649d37b00
elenegaprindashvili <elenegaprindashvili0@gmail.com>
2024-11-03 18:33:15 +0400

    commit

---
+++ level 01/homework/homework.py
@@ -0,0 +1,29 @@
+name = "elene" 
+#name არის ცვლადი
+# = ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "elene" არის ცვლადისთვის მნიშვნელობა
+surname = "gaprindashvili"
+
+#print(name)
+
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+
+name = "elene" #ეს არის str (string) ტიპის ცვლადი
+age = 13 # ეს არის int (integer) მთელი რიცხვი
+height = 1.67 #ეს არის float ტიპის ცვლადი (ათწილადი)
+ #boolean (bool) ტიპის ცვლადი
+knows_programming = True   #true ან False
+is_ugly = False #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+isUgly = False # ჯავასკრიპტული camelcase
+print(name + " " + surname)
+
+print(name + age)
+print(type(age))
+print(type(height))
+print(type(surname))
+print(type(knows_programming))
+print(name +  "  " + str(age))
+
+
+
