2468f68cdfbf49832d9726f7a9d74bd844a78391
elenegaprindashvili <elenegaprindashvili0@gmail.com>
2024-12-31 13:39:20 +0400

    commit

---
+++ level 32/classwork/classwork.py
@@ -0,0 +1,27 @@
+def opposite(number):
+    return -number
+
+print(opposite(1))   
+print(opposite(14))  
+print(opposite(-34)) 
+print(opposite(2.5)) 
+print(opposite(-0.75)) 
+
+
+
+def repeat_string(n, s):
+    return s * n
+
+print(repeat_string(6, "I"))        
+print(repeat_string(5, "Hello"))  
+
+
+def greet():
+    return "Hello, World!"
+print(greet())  
+
+
+
+
+
+
