51bd1827675e72c98c703a9c0055c35918cce369
elenegaprindashvili <elenegaprindashvili0@gmail.com>
2025-01-10 20:03:38 +0400

    commit

---
+++ level 34/classwork/classwork.py
@@ -0,0 +1,5 @@
+favorite_movies = ("anabel", "anna with a", "christmase", "The Dark Knight", "Gladiator", "pick oclock")
+first_movie, *other_movies = favorite_movies
+
+print("First movie:", first_movie)
+print("Other movies:", other_movies)
